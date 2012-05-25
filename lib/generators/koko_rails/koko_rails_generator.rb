30a31924dd0c8d0cf6749287a246e94055d85220
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-05-25 11:26:37 +0200

    fix

--- lib/generators/koko_rails/koko_rails_generator.rb
+++ lib/generators/koko_rails/koko_rails_generator.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class KokoRailsGenerator < Rails::Generators::NamedBase
   desc "This generator creates an awesome Jarzębina based layout"
   source_root File.expand_path('../templates', __FILE__)
